d8c261c55e1deaa8b73b8d1694baeed966f59e0d
iWarriorYT <35893389+iWarriorYT@users.noreply.github.com>
2018-08-23 04:52:54 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,20 +18,25 @@ client.on("ready", () => {
 client.on("message", (message) => {
     if(message.author.bot) return;
   if(message.channel.id === "479608769152548866") {
-    if (!message.guild.roles.exists("name", "[ Destek Ekibi ]")) return message.channel.send(`Bu Sunucuda '**Destek Ekibi**' rolünü bulamadım bu yüzden ticket açamıyorum \nEğer sunucu sahibisen, Destek Ekibi Rolünü oluşturabilirsin.`);
+    if (!message.guild.roles.exists("name", "Destek Ekibi")) return message.channel.send(`Bu Sunucuda '**Destek Ekibi**' rolünü bulamadım bu yüzden ticket açamıyorum \nEğer sunucu sahibisen, Destek Ekibi Rolünü oluşturabilirsin.`);
     if (message.guild.channels.exists("name", "destek-" + message.author.id)) { 
     message.delete();
     message.author.send(`Zaten açık durumda bir ticketin var.`)
         return;
     }
     message.guild.createChannel(`destek-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "[ Destek Ekibi ]");
+        let role = message.guild.roles.find("name", "Destek Ekibi");
+        let role3 = message.guild.roles.find("name", "Destek Lideri");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
             SEND_MESSAGES: true,
             READ_MESSAGES: true
         });
         c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(role3, {
             SEND_MESSAGES: false,
             READ_MESSAGES: false
         });
@@ -71,20 +76,25 @@ client.on("message", (message) => {
 
 if (message.content.toLowerCase() === `${prefix}ticket`) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "[ Destek Ekibi ]")) return message.channel.send(`Bu Sunucuda '**Destek Ekibi**' rolünü bulamadım bu yüzden ticket açamıyorum \nEğer sunucu sahibisen, Destek Ekibi Rolünü oluşturabilirsin.`);
+    if (!message.guild.roles.exists("name", "Destek Ekibi")) return message.channel.send(`Bu Sunucuda '**Destek Ekibi**' rolünü bulamadım bu yüzden ticket açamıyorum \nEğer sunucu sahibisen, Destek Ekibi Rolünü oluşturabilirsin.`);
     if (message.guild.channels.exists("name", "destek-" + message.author.id)) { 
     message.delete();
     message.author.send(`Zaten açık durumda bir ticketin var.`)
         return;
     }
     message.guild.createChannel(`destek-${message.author.id}`, "text").then(c => {
-        let role = message.guild.roles.find("name", "[ Destek Ekibi ]");
+        let role = message.guild.roles.find("name", "Destek Ekibi");
+        let role3 = message.guild.roles.find("name", "Destek Ekibi");
         let role2 = message.guild.roles.find("name", "@everyone");
         c.overwritePermissions(role, {
             SEND_MESSAGES: true,
             READ_MESSAGES: true
         });
         c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(role3, {
             SEND_MESSAGES: false,
             READ_MESSAGES: false
         });
